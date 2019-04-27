89f502a01fb29a78e5529939b43122d48af882c5
peter-keller <keller.f.peter@gmail.com>
2019-04-28 00:44:30 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTodo = document.getElementById("create-task-form");
+  const todoList = document.getElementById("tasks");
+
+  newTodo.addEventListener("submit", e => handleSubmit(e, todoList));
 });
+
+handleSubmit = (e, todoList) => {
+  e.preventDefault();
+  let listItem = document.createElement("li");
+  listItem.innerHTML = `
+    ${e.target["new-task-description"].value}
+    <button>X</button>
+  `;
+  listItem.querySelector("button").addEventListener("click", e => {
+    e.target.parentNode.remove();
+  });
+
+  todoList.appendChild(listItem);
+};
