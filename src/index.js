042cfa3e0e70bf580036dd5a96a694e2f79f9655
github username <github email address>
2019-04-29 12:51:00 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+const formEl = document.querySelector('#create-task-form')
+
+const taskEl = document.querySelector('#tasks')
+
+formEl.addEventListener('submit' , (event)=>{
+  event.preventDefault()
+  
+  const liEl = document.createElement('li')
+  const dateLi = document.createElement('li')
+
+  const newTask = formEl["new-task-description"].value
+  const dateEl = formEl["date"].value
+
+  liEl.innerText = newTask  + " " + dateEl
+  // dateLi.innerText = dateEl
+  
+	
+  taskEl.append(liEl)
+  
+  formEl.reset()
+
+	
+	
+	
+})
+  
 });
+
+// const formEl = document.querySelector('#create-task-form')
+
+// const taskEl = document.querySelector('#tasks')
+
+// formEl.addEventListener('submit' , (event)=>{
+//   event.preventDefault()
+  
+//   const liEl = document.createElement('li')
+//   const dateLi = document.createElement('li')
+
+//   const newTask = formEl["new-task-description"].value
+//   const dateEl = formEl["date"].value
+
+//   liEl.innerText = newTask  + " " + dateEl
+//   // dateLi.innerText = dateEl
+  
+	
+//   taskEl.append(liEl)
+  
+//   formEl.reset()
+
+	
+	
+	
+// })
